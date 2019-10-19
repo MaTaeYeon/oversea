296d85d5ceb81d17e45ca5606632d96616895313
unknown <qiangsheng.she@beibei.com>
2019-10-19 21:39:34 +0800

    update

--- application/controllers/Seller.php
+++ application/controllers/Seller.php
@@ -45,6 +45,9 @@ class Seller extends Api_Controller {
             $need->status = $seller_order_did_maps[$need->id]->status;
             $need->oid = $seller_order_did_maps[$need->id]->id;
         }
+        uasort($needs_list, function ($a, $b) {
+            return $a->oid > $b->oid ? -1 : 1;
+        });
         $this->response_success($needs_list);
     }
 
