5b9cf2e36e0031ed02e24d632daa125043d119f9
huimiao <411787243@qq.com>
2019-10-19 10:44:16 +0800

    update

---
+++ application/controllers/Needs.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: huimiao
+ * Date: 2019/10/19
+ * Time: 10:32 AM
+ */
+class Needs_ extends Api_Controller {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index(){
+        echo 'Hello World!';
+    }
+
+}
